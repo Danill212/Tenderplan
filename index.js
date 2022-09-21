ad27b76aa012a3c00ca0a8161a4d0d46300254bb
Danil <102981459+Danill212@users.noreply.github.com>
2022-09-21 17:16:36 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Release 0
+let student={};
+student.firstName= "Petr",
+student.lastName="Pervia", 
+student.firstName ="Petya",
+delete student.firstName
+
+
+// Release 1
+
+let group =[]
+group.push(student)
+group.push({firstName:"Igor", lastName:"Petrov"}, {firstName:"Danil", lastName:"Zemtsov"})
